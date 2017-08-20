ed28f7bc5c6b43362d5e958dc6a4d49f1ff5df01
herryben <397283864@qq.com>
2017-08-20 19:42:34 +0800

    add other data of thicker

--- manager.py
+++ manager.py
@@ -5,7 +5,7 @@ from common.emailhelper import MailHelper
 from config import app_config
 data = {}
 try:
-  with open('/home/haorui/Bcc-Minitor/data', 'r') as f:
+  with open(app_config['data_path'], 'r') as f:
     data = pickle.load(f)
 except Exception as e:
   pass
@@ -14,12 +14,12 @@ url = 'https://yunbi.com//api/v2/tickers/bcccny.json'
 response = requests.request("GET", url, timeout=5)
 result = json.loads(response.text)
 buy = float(result['ticker']['buy'])
-print buy, 'buy'
+print buy, 'buy', result
 if abs( buy - data.get('last_buy', 0)) > app_config['buy_price_delta']:
   for k, v in app_config['email'].items():
     MailHelper.send_email(subject='the current buy price is %s' % buy,
-      content='the current buy price is %s' % buy, from_addr='18647246574@163.com',
+      content='the bbc data is %s' % result, from_addr='18647246574@163.com',
       to_addr=v, nick_name='Herry')
 data['last_buy'] = buy
-with open('/home/haorui/Bcc-Minitor/data', 'w') as f:
+with open(app_config['data_path'], 'w') as f:
   pickle.dump(data, f)
